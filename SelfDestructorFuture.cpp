d5360a7e31e31e2e8239a071ddc8cf65efb555a9
pedram <mirelmipedram@gmail.com>
2022-12-18 23:15:55 +0330

    removed unnecessary files from old design

--- SelfDestructorFuture.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by pedram on 6/22/22.
-//
-
-#include "SelfDestructorFuture.h"
--- SelfDestructorFuture.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by pedram on 6/22/22.
-//
-#include <vector>
-#pragma once
-//
-//class SelfDestructorFuture {
-//    SelfDestructorFuture()
-//};
